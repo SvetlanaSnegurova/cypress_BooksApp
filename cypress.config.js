4c0c20dd59dccd5c04cac806e21090aa7a0a64ff
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-10-19 15:22:52 +0300

    change json and added tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,21 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   retries: 1,
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
   },
-})
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
